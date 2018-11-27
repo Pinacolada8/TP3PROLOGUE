90c7ae32747dcd2722d5bd62ce8476f1e7e9fbe2
Pinacolada8 <39754336+Pinacolada8@users.noreply.github.com>
2018-11-27 16:18:48 -0200

    Update prolog.pl

--- prolog.pl
+++ prolog.pl
@@ -1,2 +1,6 @@
 nelementos([], 0).
 nelementos([_|L],S) :- nelementos(L,K), S is K+1.
+
+maior([X],X).
+maior([A,B],M) :- A > B ->  M = A;M = B. 
+maior([A|B],M) :- maior(B,X),(    A > X ->  M = A; M = X ).
