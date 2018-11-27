9c7c7dd9af4a9f215d23e762d1513aa955899936
Pinacolada8 <39754336+Pinacolada8@users.noreply.github.com>
2018-11-27 16:32:51 -0200

    Update prolog.pl

--- prolog.pl
+++ prolog.pl
@@ -4,3 +4,8 @@ nelementos([_|L],S) :- nelementos(L,K), S is K+1.
 maior([X],X).
 maior([A,B],M) :- A > B ->  M = A;M = B. 
 maior([A|B],M) :- maior(B,X),(    A > X ->  M = A; M = X ).
+
+soma([A],A).
+soma([A|B],S) :- soma(B,X), S is A + X.
+
+medio(A,M) :- nelementos(A,Y), soma(A,X), M is X/Y.
