f4c3846220c3b053f09bf7dc273a55e35fced3f8
Pinacolada8 <39754336+Pinacolada8@users.noreply.github.com>
2018-11-29 15:05:25 -0200

    Create prologGostoso.pl

---
+++ prologGostoso.pl
@@ -0,0 +1,21 @@
+nelementos([], 0).
+nelementos([_|L],S) :- nelementos(L,K), S is K+1.
+
+maior([X],X).
+maior([A,B],M) :- A > B ->  M = A;M = B. 
+maior([A|B],M) :- maior(B,X),(    A > X ->  M = A; M = X ).
+
+soma([A],A).
+soma([A|B],S) :- soma(B,X), S is A + X.
+
+medio(A,M) :- nelementos(A,Y), soma(A,X), M is X/Y.
+
+inserirFim(A, [], [A]).
+inserirFim(A, [X|B], [X|C]) :- inserirFim(A, B, C).
+
+ultimo([H|[]],H).
+ultimo([_|T],U) :- ultimo(T,U).
+
+adjacente(A,B,[B,A|_]).
+adjacente(A,B,[A,B|_]).
+adjacente(A,B,[_|T]) :- adjacente(A,B,T).
